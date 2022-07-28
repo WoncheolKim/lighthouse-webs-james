0080a9d97ca0e0e6317def572fb2aad2c281e84e
Woncheol Kim <icijiy@gmail.com>
2022-07-28 14:56:00 -0700

    Lighthouse practice

--- focal/functions/callbacks-intro/findWaldo.js
+++ focal/functions/callbacks-intro/findWaldo.js
@@ -1,12 +1,27 @@
-const myFn = function() {
-  console.log("I am function.");
+// const myFn = function() {
+//   console.log("I am function.");
+// }
+
+// myFn.someAttribute = 42;
+// console.log(myFn.someAttribute);
+
+// function runner(f) {
+//   f();
+// }
+
+// runner(myFn);
+
+function findWaldo (index, found) {
+  for (let i = 0; i < index.length; i++) {
+    let name = index[i];
+    if (name === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
 }
 
-myFn.someAttribute = 42;
-console.log(myFn.someAttribute);
-
-function runner(f) {
-  f();
+const actionWhenFound = function(index) {
+  console.log(`Found Waldo at index ${index}!`);
 }
 
-runner(myFn);
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
