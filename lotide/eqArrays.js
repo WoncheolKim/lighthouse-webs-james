8e88bc44326dbe73341b14f80176cd8c644bdadd
Woncheol Kim <icijiy@gmail.com>
2022-07-27 19:16:03 -0700

    linted

--- lotide/eqArrays.js
+++ lotide/eqArrays.js
@@ -8,13 +8,7 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(firstArr, secondArr) {
-  console.log('firstArr', firstArr)
-  console.log('secondArr', secondArr)
   for (let i = 0; i < firstArr.length; i++) {
-    console.log('index', i)
-    console.log('i value', firstArr[i])
-    console.log('second i value', secondArr[i])
-    console.log(firstArr[i] === secondArr[i])
     if (firstArr[i] !== secondArr[i]) {
       return false;
     }
@@ -22,15 +16,7 @@ const eqArrays = function(firstArr, secondArr) {
   return true;
 }
 
-
-// eqArrays([1, 2, 3], [1, 2, 3]) // => true
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
