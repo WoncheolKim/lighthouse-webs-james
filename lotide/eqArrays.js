0fae6517aa54e260927ca73b77f4994f1809d1bf
Woncheol Kim <icijiy@gmail.com>
2022-07-27 19:13:38 -0700

    eqArrays practice

--- lotide/eqArrays.js
+++ lotide/eqArrays.js
@@ -7,8 +7,30 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Vancouver", "Raincouver");
-assertEqual(2, 4);
+const eqArrays = function(firstArr, secondArr) {
+  console.log('firstArr', firstArr)
+  console.log('secondArr', secondArr)
+  for (let i = 0; i < firstArr.length; i++) {
+    console.log('index', i)
+    console.log('i value', firstArr[i])
+    console.log('second i value', secondArr[i])
+    console.log(firstArr[i] === secondArr[i])
+    if (firstArr[i] !== secondArr[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+// eqArrays([1, 2, 3], [1, 2, 3]) // => true
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
