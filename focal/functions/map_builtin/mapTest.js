a1ead91924bbd6972b76befd73b3f63a0ed5349b
Woncheol Kim <icijiy@gmail.com>
2022-07-28 22:56:58 -0700

    Lighthouse mapTest

--- focal/functions/map_builtin/mapTest.js
+++ focal/functions/map_builtin/mapTest.js
@@ -1,4 +1,7 @@
 const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
 
+const houseLength = lighthouses.map(location => location.length);
+console.log(houseLength);
+
 // write code here to use map
 // It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
