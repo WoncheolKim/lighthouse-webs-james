a292a21368d80709490b7880ec6036e5f7dc3633
Woncheol Kim <icijiy@gmail.com>
2022-07-29 09:25:52 -0700

    Lighthouse practice

---
+++ lotide/map.js
@@ -0,0 +1,22 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+
+// //temporary code:
+// console.log('array: ', array);
+// console.log('callback: ', callback);
+  const results = [];
+
+  for (let item of array) {
+// temporary code:
+// console.log('item BEFORE: ', item);
+// console.log('item AFTER: ', callback(item));
+// console.log('---');
+
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
