93de3d35f915542afbf61b11c6a1592955926fb4
Woncheol Kim <icijiy@gmail.com>
2022-07-28 22:44:19 -0700

    Lighthouse practice

--- lotide/countLetters.js
+++ lotide/countLetters.js
@@ -7,3 +7,30 @@
 //   }
 // };
 
+const countLetters = function(string) {
+  const resultObject = {}
+  for (let i = 0; i < string.length; i++) {
+    if (resultObject[string[i]]) {
+      resultObject[string[i]] = resultObject[string[i]] + 1;
+    } else {
+      resultObject[string[i]] = 1
+    }
+  }
+  console.log(resultObject);
+  return resultObject;
+}
+
+countLetters("lighthouse")
+
+// bring string, 
+
+// make empty object to store key:string & value:number 
+
+// counting each letters 
+
+// loop the string + counting
+
+// add key value pair
+
+// return object
+
