7a27242290a159023289ba3e518f2329365f2b35
Woncheol Kim <icijiy@gmail.com>
2022-07-31 17:26:57 -0700

    Lighthouse practice

---
+++ lotide/findKey.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return false;
+  }
+};
+
+const findKey = function(object, callback) {
+  const keysArray = Object.keys(object);
+  for (let key of keysArray) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1), "Blue Hill");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 4 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "elBulli");
+
--- lotide/takeUntil.js
+++ lotide/takeUntil.js
@@ -1,15 +1,14 @@
 const takeUntil = function(array, callback) {
   let output = [];
   for (let item of array) {
-
     if (callback(item) === true) {
       return output;
     } else {
       output.push(item);
     }
   } return output;
-  
 }
+
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
