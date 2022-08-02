8576f3d3e2daded6bf8a685bc15ea271a454d05b
Woncheol Kim <icijiy@gmail.com>
2022-08-01 20:30:46 -0700

    Lighthouse practice

--- lotide/eqArrays.js
+++ lotide/eqArrays.js
@@ -17,9 +17,9 @@ const eqArrays = function(firstArr, secondArr) {
 }
 
 // Test Code
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 
 module.exports = eqArrays;
