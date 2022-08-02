cc28fe6fbcb1644e3fe8aec867cbff0d2d51aadd
Woncheol Kim <icijiy@gmail.com>
2022-08-02 07:55:10 -0700

    Lighthouse practice

--- async/hello_timeout.js
+++ async/hello_timeout.js
@@ -1,9 +1,21 @@
 function threeTimer (seconds) {
   setTimeout(
     function() {
-      console.log("4311o th3r3 w0r1d");
+      console.log("4311o");
     },
     seconds * 3000
+  );
+  setTimeout(
+    function() {
+      console.log("th3r3");
+    },
+    seconds * 6000
+  );
+  setTimeout(
+    function() {
+      console.log("w0r1d");
+    },
+    seconds * 9000
   );
 }
 
